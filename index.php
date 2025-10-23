8808b33f54e9bb7d213d2d8c9ed2b9ea2f4f3223
TheSamuelDopke <samueloficial1@hotmail.com>
2025-10-22 21:54:26 -0300

    commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog do Samo</title>
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header>
+        <h1>Blog do Samo</h1>
+        <a href="criar_post.php"><button>Criar post</button></a>
+    </header>
+
+    <main>
+        <h2>Posts:</h2>
+        <ul>
+            <li>Não há nenhum post.</li>
+        </ul>
+    </main>
+</body>
+</html>
