7e97a7296bcb083648d7f2f8c05cdba9518fcbdb
TheSamuelDopke <samueloficial1@hotmail.com>
2025-11-17 13:04:40 -0300

    commit

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 <?php
 
 //Define a conexão com o banco de dados:
-define('HOST', 'localhost');
-define('USUARIO', 'root');
+define('HOST', '');
+define('USUARIO', '');
 define('SENHA', '');
-define('DB', 'crud_bd');
+define('DB', '');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar ao banco de dados!')
 
